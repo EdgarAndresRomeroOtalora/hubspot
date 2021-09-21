aba4394b5851d2df8970c7bf2048794ef7eb0b63
Edgar Romero <edgar.romero@softtek.com>
2021-09-21 11:28:56 -0500

    probando commit

--- src/main/java/es/hubspot/propiedades/client/UsuarioClient.java
+++ src/main/java/es/hubspot/propiedades/client/UsuarioClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 //https://api.hubapi.com/contacts/v1/contact/vid/1001/profile?hapikey=afe6b7c4-d592-447c-973f-a2c65eb9e906   VER POR ID
 //https://api.hubapi.com/contacts/v1/contact/vid/1001?hapikey=afe6b7c4-d592-447c-973f-a2c65eb9e906
 
-@FeignClient(value="UsuarioClient",url="https://api.hubapi.com/contacts/v1/contact/vid/901/profile?hapikey=afe6b7c4-d592-447c-973f-a2c65eb9e906")
+@FeignClient(value="UsuarioClient",url="https://api.hubapi.com/contacts/v1/contact/?hapikey=afe6b7c4-d592-447c-973f-a2c65eb9e906")
 
 public interface UsuarioClient {
 	@RequestMapping(method = RequestMethod.POST, value = "", consumes = "application/json")
